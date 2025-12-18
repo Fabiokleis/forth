92d85906e399c9e5e6a604e8863a697e031eb5e2
fabiokleis <fabiohkrc@gmail.com>
2025-12-17 21:31:03 -0300

    created repl

--- forth.hs
+++ forth.hs
@@ -1,10 +1,10 @@
 module Forth where
 
--- simple evaluator for subset forth 
+-- simple evaluator for forth subset
 
 data Expr =
   Lit Int
-  | Sym String -- symbol defs
+  | Word String -- word definition
   | Add
   | Sub
   | Mul
@@ -49,18 +49,18 @@ parseExpr "OVER" = Right Over
 parseExpr "DROP" = Right Drop
 parseExpr "CR" = Right Cr
 parseExpr "LF" = Right Lf
-parseExpr any = Left ("undefined expression: " ++ "'" ++ any ++ "'")
+parseExpr any = Left ("**undefined expression**: " ++ "'" ++ any ++ "'")
 
 parse :: [String] -> Program -> Table -> Either (String, Program, Table) (Program, Table)
 parse [] prog table = Right (prog, table)
-parse (":":symbol:rest) prog table =
-  case lookup symbol table of
-    Just _ -> Left ("duplicate name: " ++ "'" ++ symbol ++ "'", prog, table)
+parse (":":word:rest) prog table =
+  case lookup word table of
+    Just _ -> Left ("**duplicate name**: " ++ "'" ++ word ++ "'", prog, table)
     Nothing ->
       case parse exprs [] table of
         Left err -> Left err
         Right (iexprs, itable) ->
-          parse next prog ((symbol, iexprs):itable) 
+          parse next prog ((word, iexprs):itable) 
       where
         exprs = takeWhile (\x -> x /= ";") rest -- check for nested defs
         next = tail (dropWhile (\x -> x /= ";") rest) -- fails on unfinished defs 
@@ -72,17 +72,17 @@ parse (token:rest) prog table =
              Right expr -> parse rest (expr : prog) table
              Left err ->
                case lookup token table of
-                 Just _ -> parse rest ((Sym token) : prog) table
+                 Just _ -> parse rest ((Word token) : prog) table
                  Nothing -> Left (err, prog, table)
 
-parseTokens :: String -> Either (String, Program, Table) (Program, Table)
-parseTokens code =
-  case parse (words code) [] [] of
+parseTokens :: String -> Table -> Either (String, Program, Table) (Program, Table)
+parseTokens code reftable =
+  case parse (words code) [] reftable of
     Right (prog, table) -> Right ((reverse prog), table)
     Left e -> Left e
 
 stackUnderflow :: Expr -> String
-stackUnderflow expr = "stack underflow: " ++ show expr
+stackUnderflow expr = "**stack underflow**: " ++ show expr
 
 eval :: Evaluator -> IO Evaluator
 eval state@(Evaluator [] _ _) = return state
@@ -129,51 +129,30 @@ eval (Evaluator (expr:rest) stack table) =
           let res = if x == y then -1 else 0 
           in eval (Evaluator rest (res:xs) table)
         _ -> error $ stackUnderflow expr
-    Sym key ->
+    Word key ->
       case lookup key table of
         Just defs -> eval (Evaluator ((reverse defs) ++ rest) stack table)
-        Nothing -> error $ "undefined word: " ++ "'" ++ key ++ "'"
+        Nothing -> error $ "**undefined word**: " ++ "'" ++ key ++ "'"
   where
     binaryOp f =
       case stack of
         (y:x:xs) -> eval (Evaluator rest (f x y : xs) table)
-        _ -> error $ "stack underflow binary operator: " ++ show expr
+        _ -> error $ "**stack underflow binary operator**: " ++ show expr
+  
+interpreter :: Evaluator -> IO ()
+interpreter state@(Evaluator _ estack etable) =
+  do
+    line <- getLine
+    case parseTokens line etable of
+      Left (err, program, table) -> do
+        putStrLn $ "program: " ++ show program
+        putStrLn $ "table: " ++ show table
+        putStrLn $ "error: " ++ err
+        interpreter state
+      Right (iprog, itable) -> do
+        evaluated <- eval (Evaluator iprog estack itable)
+        putStrLn "  ok"
+        interpreter evaluated
 
-progs :: [String]
-progs = [
-      "1",
-      "3 4 + 9 -",
-      "10 5 - 2 *",
-      "20 5 / 3 +",
-      "1 2 3 * -",
-      "5 2 10 /",
-      "1 2 3 +",
-      "1 + 3",
-      "1 2 + : defined 10 ; 1",
-      ": empty ;",
-      ": def1 1 ; : def2 2 ; : def3 3 ;  def1 def2 def3 def2 +",
-      ": f 1 ; f dup +",
-      ": r 5 ; : l 10 ; l r =",
-      ": l 5 ; : r l 5 + ; r 10 =",
-      ": l 10 ; : r dup l + ; 2 l * r =",
-      ": l 1 ; : r dup dup ; l r",
-      ": f 102 ; : o 111 ; : r 114 ; : t 116 ; : h 104 ; : out f emit o emit r emit t emit h emit ; out 10 emit"
-      ]
-        
 main :: IO ()
-main = do
-  let prog = (!!) progs 16
-  putStrLn "------------- Forth ------------"
-  putStrLn $ "source: " ++ "'" ++ prog ++ "'"
-  case parseTokens prog of
-    Left (err, p, t) -> do
-      putStrLn $ "program: " ++ show p
-      putStrLn $ "table: " ++ show t
-      putStrLn $ "error: " ++ err
-    Right (iprog, itable) -> do
-      putStr "stdout: "
-      (Evaluator program stack table) <- eval (Evaluator iprog [] itable)
-      putStrLn $ "table: " ++ show table
-      putStrLn $ "program: " ++ show iprog
-      putStrLn $ "stack: " ++ show stack
-  putStrLn "--------------------------------" 
+main = interpreter (Evaluator [] [] [])
